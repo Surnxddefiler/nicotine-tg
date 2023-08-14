0f0d8d8aff0c8d0076d69dc100337916aaea3417
Еж <easzyXD@gmail.com>
2023-08-14 23:31:20 +0300

    first commit

--- index.js
+++ index.js
@@ -38,7 +38,9 @@ bot.on('message', async (msg) => {
     if (msg.web_app_data) {
         try {
             const data = JSON.parse(msg.web_app_data.data)
-            await bot.sendMessage(chatId, 'Заказ успешно оформлен, курьер - @ravenonstop в скором времени с вами свяжется ✅')
+            await bot.sendMessage(chatId, `Заказ успешно оформлен, курьер - @ravenonstop в скором времени с вами свяжется ✅ \n Ваш заказ: \n ${data.val.time} /n ${data.cart.map((el, i) => {
+                return `\n${el.mark} ${el.name} ${el.nicotine} `
+                })}`)
             console.log(data)
             await bot.sendMessage(-623730102, `\n${data.val.time} \n${data.val.phone} \n${data.place} \n${msg.from.username ? `@${msg.from.username}` :`-`} ${data.cart.map((el, i) => {
             return `\n${el.mark} ${el.name} ${el.nicotine} `
